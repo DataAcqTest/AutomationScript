7c340776f6e3deedef1a68e283f008f1256acd7b
Pedro Debevere <pedro.debevere@skyline.be>
2023-03-17 09:49:42 +0100

    added unit test

---
+++ HelloWorld_1Tests/UtilityTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld_1;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_1.Tests
+{
+	[TestClass()]
+	public class UtilityTests
+	{
+		[TestMethod()]
+		public void ConvertTest()
+		{
+			// Arrange
+			string input = "a";
+
+			// Act
+			var result = Utility.Convert(input);
+
+			// Assert
+			Assert.AreEqual("ConvertedA", result);
+		}
+	}
+}
